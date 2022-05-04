c60c56ac00d92d50be6dbf2a3b704049781e7918
okamoto-tosei <tosei393939@gmail.com>
2022-05-04 19:55:20 +0900

    env setting

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
+require('dotenv').config({ path: `.env.${process.env.ENVIRONMENT}` });
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
